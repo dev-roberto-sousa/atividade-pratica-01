1be041b9fc0b665c1eb8931330f6fd5ab1ad89ce
Roberto Sousa <dev.roberto.sousa@exemplo.com>
2025-09-18 16:27:58 -0300

    Atividade concluída.

---
+++ calculadora_de_soma.py
@@ -0,0 +1,6 @@
+numero_1 = 12
+numero_2 = 14
+
+resultado = numero_1 + numero_2
+
+print(f"O resultado da soma entre {numero_1} e {numero_2} é: {resultado}.")
---
+++ calculadora_de_volume.py
@@ -0,0 +1,13 @@
+"""
+ Comprimento: 12 cm
+* Largura: 14 cm
+* Altura: 20 cm
+"""
+
+comprimento = 12
+largura = 14
+altura = 20
+
+volume = comprimento * largura * altura
+
+print(f"O volume da caixa em cm³ é: {volume}cm³")
